fa8198472724a51b4d5bcb52e7ec427e24a7568d
Sean Maynard <sean@swagritech.com.au>
2026-03-14 14:07:05 +0800

    Fix OAuth callback routing to single app handler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        manifestPlaceholders["appAuthRedirectScheme"] = "swatops"
+        manifestPlaceholders["appAuthRedirectScheme"] = "swatopsappauthunused"
 
         buildConfigField("String", "API_BASE_URL", "\"https://api.swagritech.com.au/api/\"")
         buildConfigField("String", "AUTH_SCOPE", "\"openid profile email offline_access User.Read\"")
